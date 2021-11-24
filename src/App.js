4fbbf8b3f8e68c5c63b577e2f1d1bd50324681a0
Tommosaurus <87076987+Tommosaurus@users.noreply.github.com>
2021-11-24 17:47:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  Stack,
+  Image,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+const axios = require("axios").default;
 
 function App() {
+  const [deals, setDeals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://6177b8b59c328300175f5adc.mockapi.io/api/test/deals")
+      .then((result) => {
+        setDeals(result.data.deals);
+      });
+  }, []);
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table variant="striped" colorScheme="white">
+        {/* <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Price per month</Th>
+            <Th>Speed</Th>
+          </Tr>
+        </Thead> */}
+
+        <Tbody>
+          {deals.map((item) => {
+            return (
+              <Tr>
+                <Td>
+                  <Image boxSize="60px" src={item.provider_logo_image_url} />
+                </Td>
+                <Td>{item.provider_name}</Td>
+                <Td>{`£${item.monthly_price}`}</Td>
+                <Td>{`${item.internet_speed}Mbps`}</Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
     </div>
   );
 }
